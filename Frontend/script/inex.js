6a61dd850a67848aea9c39dbf5805f23d2638fd7
Aman Verma <amancreative9598@gmail.com>
2023-01-29 06:16:03 +0530

    index page

---
+++ Frontend/script/inex.js
@@ -0,0 +1,18 @@
+
+// Using GSAP for animation
+
+// NAVBAR
+TweenMax.from(".navbar", 1.5,  {
+    delay:2.5,
+    opacity:0,
+    y:20,
+})
+ // TEXT
+ TweenMax.from(".text h1 .text-bar", 1.5,  {
+    delay:1.6,
+    y:"135%",
+})
+TweenMax.from(".text p .text-bar", 1.5,  {
+    delay:2,
+    y:"100%",
+})
