8201aeb2f6b9b15294377c1b44e367e3f50ba587
Aman Verma <amancreative9598@gmail.com>
2023-02-01 23:44:30 +0530

    new work

---
+++ Backend/shopeasy/src/main/java/com/shopeasy/ShopeasyApplication.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeasyApplication.class, args);
+	}
+
+}
