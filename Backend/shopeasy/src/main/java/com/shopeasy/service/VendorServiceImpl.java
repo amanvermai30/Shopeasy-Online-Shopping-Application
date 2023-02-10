f51d7e03e7a475a19286c6d473f0affc778272b6
Aman Verma <amancreative9598@gmail.com>
2023-02-10 08:43:19 +0530

    backend work vendor controller creating vendor account  api

--- Backend/shopeasy/src/main/java/com/shopeasy/service/VendorServiceImpl.java
+++ Backend/shopeasy/src/main/java/com/shopeasy/service/VendorServiceImpl.java
@@ -1,5 +1,38 @@
 package com.shopeasy.service;
 
-public class VendorServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.shopeasy.exception.PersonalInfoException;
+import com.shopeasy.exception.VendorException;
+import com.shopeasy.model.Customer;
+import com.shopeasy.model.Vendor;
+import com.shopeasy.repository.PersonalInfoDao;
+import com.shopeasy.repository.VendorDao;
+
+public class VendorServiceImpl implements VendorService{
+	
+	@Autowired
+	VendorDao vendorDao;
+	
+	@Autowired
+	PersonalInfoDao personalInfoDao;
+
+	@Override
+	public String createCustomerAccount(Vendor vendor) throws VendorException, PersonalInfoException {
+		// TODO Auto-generated method stub
+		
+        String outPut = "Customer Account is Not created";
+		
+		Vendor ven = vendorDao.save(vendor);
+		if(ven != null ) {
+			personalInfoDao.save(vendor.getPersonalInfo());
+			outPut = "Vendor Account is created Successfully";
+			
+		}else {
+			throw new PersonalInfoException("Error while creating vendor account: ");
+		}
+
+		return outPut;
+	}
 
 }
